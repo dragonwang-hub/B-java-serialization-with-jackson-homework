7cc9bfa921770858b5b817d67144179569fd8aa3
dragonwang-hub <915614879@qq.com>
2020-11-05 22:39:27 +0800

    [dargon] feat: finished problem 1 - EventController

---
+++ src/main/java/com/thoughtworks/capability/gtb/vo/DateSerializer.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.capability.gtb.vo;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import java.util.Date;
+
+public class DateSerializer extends StdSerializer<Date> {
+
+    protected DateSerializer() {
+        super(Date.class);
+    }
+
+
+    @Override
+    public void serialize(Date value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeNumber(value.getTime());
+    }
+}
--- src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum EventType {
   UPLOAD("U"), DOWNLOAD("D");
-
+  @JsonValue
   private String code;
 
   EventType(String code) {
     this.code = code;
   }
 }
+
+
